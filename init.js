0c2991dbb5a2f7b15c0cd9f8e7cba7c594321c7f
slavapavlutin <sl.pavlutin@gmail.com>
2018-01-17 12:59:49 +0300

    Update init script

--- init.js
+++ init.js
@@ -10,14 +10,19 @@ function foldCode(editor) {
   }
 
   if (currentGrammar === 'Babel ES6 JavaScript') {
+    // const file = editor.buffer.file;
+    // if (file.path.endsWith('.spec.js')) {
+    //   editor.foldAllAtIndentLevel(1);
+    // } else {
     editor.foldAllAtIndentLevel(0);
+    // }
   }
 
-  if (_in(currentGrammar, ['Less', 'SCSS'])) {
+  if (_contains(currentGrammar, ['Less', 'SCSS'])) {
     editor.foldAllAtIndentLevel(0);
   }
 }
 
-function _in(element, array) {
+function _contains(element, array) {
   return array.indexOf(element) >= 0 ? true : false;
 }
